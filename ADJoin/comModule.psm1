2baed55fa22ccc4967063d8b199667840ad52879
kwonjoonsang <basketlinux@nate.com>
2017-03-31 17:50:31 +0900

    AD Join Change

--- ADJoin/comModule.psm1
+++ ADJoin/comModule.psm1
@@ -1,1 +1,52 @@
-﻿Function Get-Requirement{    Param    (        $strUser    )    $bolCheck = 0    If ($strUser -eq $null)    {         If ((Test-Path .\UserInfo.txt) -eq $false)        {             $bolCheck = 1        }    }    If ((Test-Path .\Group.txt) -eq $false)    {        $bolCheck = 2    }    return $bolCheck}Function Modify-LocalGroup{    Param    (        $strDomainNM    )    $strGroups = Get-Content .\Group.txt    $strLocalGroup = "Administrators"    $strADSI = [ADSI]"WinNT://./$strLocalGroup"    $strADSI.Remove("WinNT://$strDomainNM/Domain Admins")    ForEach($strGroup in $strGroups)    {        $strADSI.ADD("WinNT://$strGroup")        Write-Host $strGroup    }}
+﻿#
+# **************************************************************************************************
+# Script Name : comModule.psm1
+# Usage
+#		- AD Join Common Module
+# Parameter
+# **************************************************************************************************
+#
+Function Get-Requirement
+{
+    Param
+    (
+        $strUser
+    )
+
+    $bolCheck = 0
+
+    If ($strUser -eq $null)
+    { 
+        If ((Test-Path .\UserInfo.txt) -eq $false)
+        { 
+            $bolCheck = 1
+        }
+    }
+
+    If ((Test-Path .\Group.txt) -eq $false)
+    {
+        $bolCheck = 2
+    }
+
+    return $bolCheck
+}
+
+Function Modify-LocalGroup
+{
+    Param
+    (
+        $strDomainNM
+    )
+
+    $strGroups = Get-Content .\Group.txt
+    $strLocalGroup = "Administrators"
+    $strADSI = [ADSI]"WinNT://./$strLocalGroup"
+
+    $strADSI.Remove("WinNT://$strDomainNM/Domain Admins")
+
+    ForEach($strGroup in $strGroups)
+    {
+        $strADSI.ADD("WinNT://$strGroup")
+        Write-Host $strGroup
+    }
+}
